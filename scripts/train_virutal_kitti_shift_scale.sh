3de786033a180baf5c9ef7c362c601f7bfb56aa3
Andrew Liao <andliao@nvidia.com>
2022-04-10 11:57:57 -0400

    shell file to python

--- scripts/train_virutal_kitti_shift_scale.sh
+++
@@ -1,5 +0,0 @@
-cd ../datasets_preparation/
-python prepare.py --datasets virtual_kitti 
-python prepare.py --datasets virtual_kitti --shift 0.2-up
-cd ../
-python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py dataset=virtual_kitti add_dataset=virtual_kitti add_dataset.path=datasets/virtual_kitti_shift-0.2-up/coco_format batch_size=16 lr_drop=100 epochs=200 use_wandb=True hydra.run.dir=/results
